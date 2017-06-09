62a816e373b6b0b1085d1521dfb5c1739bd4e1d6
liwq <liwq02@koti.cn>
2017-06-09 16:30:18 +0800

    十分动感的自定义tabBar，可以上下联动。

---
+++ KTTabBarDemo/KTTabBarDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KTTabBarDemo
+//
+//  Created by Vincent on 2017/6/9.
+//  Copyright © 2017年 李维庆. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
